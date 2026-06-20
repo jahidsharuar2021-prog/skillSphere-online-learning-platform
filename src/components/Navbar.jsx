c18e6b4575bdfc048e2b7e10d866818e8f9885d4
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-06-20 14:47:56 +0600

    added google auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 "use client";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+
+  const userdata =authClient.useSession()
+  const user=userdata.data?.user
+  
+  const handleSignOut=async()=>{
+    await authClient.signOut();
+
+  }
+
   return (
     <div className="border-b px-2 bg-white border-b border-slate-200 sticky top-0 z-50">
       <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
@@ -31,14 +42,31 @@ const Navbar = () => {
         </ul>
 
         <div className="flex gap-4">
-          <ul className="flex items-center gap-3 text-sm">
-            <li>
-              <Link href={"/signin"}>Login</Link>
-            </li>
-            <li>
-              <Link href={"/signup"}> Register</Link>
-            </li>
-          </ul>
+          {!user && (
+            <ul className="flex items-center gap-3 text-sm">
+              <li>
+                <Link href={"/signin"}>Login</Link>
+              </li>
+              <li>
+                <Link href={"/signup"}> Register</Link>
+              </li>
+            </ul>
+          )}
+
+          {user && (
+            <div className="flex gap-3">
+              {" "}
+              <Avatar size="sm">
+                <Avatar.Image
+                  alt="John Doe"
+                  src={user?.image}
+                  referrerPolicy="no-referrer"
+                />
+                <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+              </Avatar>
+              <Button onClick={handleSignOut} size="sm" variant="danger">Logout</Button>
+            </div>
+          )}
         </div>
       </nav>
     </div>
